3666a94343cc3cf460e767d380c7cfb7cd1c9889
Matin Sarahi <matinsarahi@gmail.com>
2021-05-28 19:50:11 -0400

    hover

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -10,10 +10,22 @@ const MainPage = () => {
   return (
     <StyledGridContainer>
       <StyledGrid>
-        <img className="grid1" src={lady} alt="" />
-        <img className="grid2" src={interior} alt="" />
-        <img className="grid3" src={guy} alt="" />
-        <img className="grid4" src={building} alt="" />
+        <StyledGrid1>
+          <h2>People</h2>
+          <img className="grid1" src={lady} alt="" />
+        </StyledGrid1>
+        <StyledGrid2>
+          <h2>Interiors</h2>
+          <img className="grid2" src={interior} alt="" />
+        </StyledGrid2>
+        <StyledGrid3>
+          <h2>Outdoors</h2>
+          <img className="grid3" src={guy} alt="" />
+        </StyledGrid3>
+        <StyledGrid4>
+          <h2>City</h2>
+          <img className="grid4" src={building} alt="" />
+        </StyledGrid4>
       </StyledGrid>
     </StyledGridContainer>
   );
@@ -26,14 +38,65 @@ const StyledGrid = styled(motion.div)`
   grid-row-gap: 10vh;
   grid-column-gap: 5vw;
   img {
+    opacity: 0.9;
     width: 100%;
     height: 100%;
     object-fit: cover;
     object-position: center;
   }
 `;
+
 const StyledGridContainer = styled(motion.div)`
   margin: auto;
   width: 80%;
 `;
+
+const StyledGrid1 = styled(motion.div)`
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  h2 {
+    font-size: 2.5rem;
+    position: absolute;
+    z-index: 1;
+  }
+`;
+const StyledGrid2 = styled(motion.div)`
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  h2 {
+    font-size: 2.5rem;
+    position: absolute;
+    z-index: 1;
+  }
+  img:hover {
+    opacity: 1;
+  }
+`;
+const StyledGrid3 = styled(motion.div)`
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  h2 {
+    font-size: 2.5rem;
+    position: absolute;
+    z-index: 1;
+  }
+`;
+const StyledGrid4 = styled(motion.div)`
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  h2 {
+    font-size: 2.5rem;
+    position: absolute;
+    z-index: 1;
+  }
+`;
+
 export default MainPage;
